3a87ed5e0b5167f1735d0296557fad3fab2f4211
vaisanem <miikkamv@gmail.com>
2019-02-07 20:55:09 +0200

    3.1

---
+++ puhelinluettelo-backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const app = express()
+
+const contacts = [
+  {
+    id: 1,
+    name: "Hyvä ystävä",
+    number: "040-2364782"
+  },
+  {
+    id: 2,
+    name: "Toinen ystävä",
+    number: "040-3364782"
+  },
+  {
+    id: 3,
+    name: "Ystävä Hyvä",
+    number: "040-2364783"
+  }
+]
+
+app.get("/api/contacts", (req, res) => {
+  res.json(contacts)
+})
+
+const PORT = 3001
+app.listen(PORT)

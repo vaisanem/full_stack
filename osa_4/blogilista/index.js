ca1140b637765a34bc28960cf8c75d71585ff7c4
vaisanem <miikkamv@gmail.com>
2019-02-15 12:50:38 +0200

    model

--- osa_4/blogilista/index.js
+++ osa_4/blogilista/index.js
@@ -5,15 +5,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require('./models/blog')
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true })

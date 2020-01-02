581b457e1bc3fdc74bd9aa26141568c6fc0e1ef1
vaisanem <miikkamv@gmail.com>
2020-01-02 19:49:48 +0200

    8.2

--- osa_8/kirjasto-backend/index.js
+++ osa_8/kirjasto-backend/index.js
@@ -85,10 +85,18 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    id: ID!,
+    title: String!,
+    author: String!,
+    published: Int!,
+    genres: [String]!
+  },
   type Query {
     hello: String!,
     bookCount: Int!,
-    authorCount: Int!
+    authorCount: Int!,
+    allBooks: [Book]!
   }
 `
 
@@ -96,7 +104,8 @@ const resolvers = {
   Query: {
     hello: () => { return "world" },
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 

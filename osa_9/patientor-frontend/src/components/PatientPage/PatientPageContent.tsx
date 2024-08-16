96c8aaa2fce85999612fbd64298c35829ca3711d
Miikka Väisänen <miikka.vaisanen@vincit.fi>
2024-08-16 21:48:56 +0300

    9.23

--- osa_9/patientor-frontend/src/components/PatientPage/PatientPageContent.tsx
+++ osa_9/patientor-frontend/src/components/PatientPage/PatientPageContent.tsx
@@ -1,5 +1,6 @@
 import { Patient } from '../../types';
 import GenderIcon from './GenderIcon';
+import { List, ListItem, Divider } from '@mui/material';
 
 interface Props {
   patient: Patient;
@@ -11,6 +12,24 @@ const PatientPageContent = ({ patient }: Props) => {
       <h1>{patient.name} <GenderIcon gender={patient.gender} /></h1>
       <p>ssn: {patient.ssn}</p>
       <p>occupation: {patient.occupation}</p>
+      <br />
+      <h3>Entries:</h3>
+      {patient.entries.map(entry => (
+        <div key={entry.id}>
+          <p>{entry.date} {entry.description}</p>
+          <List>
+            {entry.diagnosisCodes?.map(code => (
+              <span key={code}>
+                <ListItem key={code}>
+                  {code}
+                </ListItem>
+                  <Divider />
+              </span>
+            ))}
+          </List>
+          <Divider />
+        </div>
+      ))}
     </div>
   );
 };
--- osa_9/patientor-frontend/src/types.ts
+++ osa_9/patientor-frontend/src/types.ts
@@ -1,22 +1,65 @@
-export interface Diagnosis {
+export type Diagnosis = {
   code: string;
   name: string;
   latin?: string;
+};
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3
 }
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: 'HealthCheck';
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  }
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: 'Hospital';
+  discharge: {
+    date: string;
+    criteria: string;
+  }
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export enum Gender {
-  Male = "male",
-  Female = "female",
-  Other = "other"
+  Male = 'male',
+  Female = 'female',
+  Other = 'other'
 }
 
-export interface Patient {
+export type Patient = {
   id: string;
   name: string;
   occupation: string;
   gender: Gender;
+  entries: Entry[];
   ssn?: string;
   dateOfBirth?: string;
-}
+};
 
-export type PatientFormValues = Omit<Patient, "id" | "entries">;
+export type PatientFormValues = Omit<Patient, 'id' | 'entries'>;
